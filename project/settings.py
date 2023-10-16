85c907e9644e6853c3f7e0b585ff1ecbf9295471
Omid Shojaee <omid.shojaee@gmail.com>
2023-10-16 16:36:28 +0330

    Update STATIC_ROOT

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/usr/src/app/staticfiles/'
 STATICFILES_DIRS = [BASE_DIR / 'assets/', ]
 
 # Default primary key field type
